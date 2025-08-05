b13becebd8b20e6023c2c84c297d37840728a895
Aishkash <aishwaryakashy@gmail.com>
2025-08-05 11:18:07 +0530

    Time: 61 ms (14.48%), Space: 12.8 MB (51.06%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,18 +1,7 @@
+from collections import Counter
+
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-        sz1=len(ransomNote)
-        sz2=len(magazine)
-        if sz1>sz2:
-            return False
-        t,p=0,0    
-        while t < sz1 and p < len(magazine):
-            if ransomNote[t]==magazine[p]:
-                magazine = magazine[:p] + magazine[p+1:]
-                t+=1
-                p=0
-            else:
-                p+=1   
-            if t==sz1:
-                return True    
-        return False            
-                  
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        return all(ransom_count[char] <= magazine_count[char] for char in ransom_count)
