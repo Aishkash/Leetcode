b05375693b63dcada9c9a74715f00c798a189e94
Aishkash <aishwaryakashy@gmail.com>
2025-08-05 11:28:49 +0530

    Time: 56 ms (26.49%), Space: 12.7 MB (78.52%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -4,4 +4,8 @@ class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         ransom_count = Counter(ransomNote)
         magazine_count = Counter(magazine)
-        return all(ransom_count[char] <= magazine_count[char] for char in ransom_count)
+        
+        for letters in ransom_count:
+            if ransom_count[letters]>magazine_count[letters]:
+                return False
+        return True        
