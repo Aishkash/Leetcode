a08db7689d04d9fb4fbd41ca8877c8ec71db4453
Aishkash <aishwaryakashy@gmail.com>
2025-09-09 17:34:44 +0530

    Time: 458 ms (68.72%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,7 +2,10 @@ SELECT
     t.id,
     CASE
         WHEN t.p_id IS NULL THEN 'Root'
-        WHEN EXISTS (SELECT 1 FROM Tree c WHERE c.p_id = t.id) THEN 'Inner'
+        WHEN EXISTS (
+            SELECT 1 
+            FROM Tree c 
+            WHERE t.id=c.p_id) THEN 'Inner'
         ELSE 'Leaf'
     END AS type
 FROM Tree t;
