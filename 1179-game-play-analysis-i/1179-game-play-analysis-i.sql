28f2047c93f7f0f2b898e61d9ceb307872477dbf
Aishkash <aishwaryakashy@gmail.com>
2025-10-03 13:56:24 +0530

    Time: 636 ms (13.89%), Space: 0B (100%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -5,14 +5,14 @@
 -- and a1.player_id>b1.player_id;
 
 
--- SELECT player_id, MIN(event_date) AS first_login
--- FROM Activity
--- GROUP BY player_id;
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
 
-SELECT player_id, event_date AS first_login
-FROM (
-    SELECT player_id, event_date,
-           ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
-    FROM Activity
-) t
-WHERE rn = 1;
+-- SELECT player_id, event_date AS first_login
+-- FROM (
+--     SELECT player_id, event_date,
+--            ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
+--     FROM Activity
+-- ) t
+-- WHERE rn = 1;
