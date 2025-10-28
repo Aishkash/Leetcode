a1907a2f7cf93dd4f50d123ca47d1fb586c03eb7
Aishkash <aishwaryakashy@gmail.com>
2025-10-28 13:16:40 +0530

    Time: 2 ms (99.71%), Space: 17.7 MB (95.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,9 +3,7 @@ class Solution(object):
         ans=[0]*(n+1)
         offset=1
         for i in range(1, n + 1):
-            if offset*2==i:
-                offset=i
-            ans[i]=1+ans[i-offset]
+            ans[i] = ans[i >> 1] + (i & 1)
         return ans
             
         
