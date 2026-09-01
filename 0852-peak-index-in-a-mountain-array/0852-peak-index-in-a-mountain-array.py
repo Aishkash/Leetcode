c31653e753e4b9a3b770839c0c87e7ba7828bcfc
Aishkash <aishwaryakashy@gmail.com>
2026-09-02 00:02:04 +0530

    Time: 0 ms (100%), Space: 21.6 MB (71.84%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def peakIndexInMountainArray(self, arr):
-        left = 0
-        right = len(arr) - 1
-
-        while left < right:
-            mid = (left + right) // 2
-
+        sz=len(arr)
+        ans=0
+        left,right=0,sz-1
+        while left<right:
+            mid=(left+right)//2
             if arr[mid] < arr[mid + 1]:
-                # We are on the increasing side
                 left = mid + 1
             else:
-                # We are on the decreasing side
                 right = mid
 
         return left
